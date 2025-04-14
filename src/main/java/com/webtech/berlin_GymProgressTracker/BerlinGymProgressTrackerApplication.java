fb1dc83939bc013a975dcd455125ab4c134313ea
Biene8 <130748855+Biene8@users.noreply.github.com>
2025-04-14 12:55:56 +0200

    Project structure

---
+++ src/main/java/com/webtech/berlin_GymProgressTracker/BerlinGymProgressTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.webtech.berlin_GymProgressTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BerlinGymProgressTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BerlinGymProgressTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webtech/berlin_GymProgressTracker/BerlinGymProgressTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webtech.berlin_GymProgressTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BerlinGymProgressTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
